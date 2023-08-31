0c7e3b79e7f9b7fff105ca2b7d49daf18ff74d55
janet9876 <123964482+janet9876@users.noreply.github.com>
2023-08-31 22:35:54 +0530

    Add files via upload

---
+++ Porfolio 2/script.js
@@ -0,0 +1,16 @@
+<script>
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('header a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+</script>
